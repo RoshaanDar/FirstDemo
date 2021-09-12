46a55c4b0c1b9189d562eb96942441f887e92397
kamran <comptbaba@gmail.com>
2021-09-12 17:54:06 -0400

    Inital Commit

---
+++ src/myfirstprog/prog1.java
@@ -0,0 +1,38 @@
+package myfirstprog;
+
+import java.util.Scanner;
+
+public class prog1 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+//		String firstName = "Roshaan ";
+//		String lastName = "Dar";
+//		System.out.println("Your Name is " + firstName + lastName);
+//		
+//		int num = 1;
+//		int num2 = 2;
+//		System.out.println(num + num2);
+//		
+//		
+		
+		
+		
+		String empID;
+		String emip;
+		
+		
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter Your Full Name ");
+		empID = input.nextLine();
+		System.out.printf("Enter Your ID: ", empID);
+		emip = input.nextLine();
+		System.out.println(emip);
+	
+		
+        
+	}
+
+}
